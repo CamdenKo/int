f2992112695ff97e01afcae4a42115f9afca9ca3
Camden Ko <camden@ramp.com>
2021-07-13 14:16:52 -0400

    need the memo

--- src/components/OptionsList/hooks.ts
+++ src/components/OptionsList/hooks.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { AlertRuleType, AlertsResource, OptionValue } from "../../types";
 
 let lastId = 0;
@@ -10,68 +11,72 @@ export const useAlert = (): AlertsResource => {
   };
 };
 
-export const useListSelectOptions = (): OptionValue[] => [
-  {
-    label: "Travel",
-    value: {
-      id: 14,
-      type: AlertRuleType.SK_CATEGORY,
-    },
-  },
-  {
-    label: "Electronics",
-    value: {
-      id: 19,
-      type: AlertRuleType.SK_CATEGORY,
-    },
-  },
-  {
-    label: "SaaS",
-    value: {
-      id: 92,
-      type: AlertRuleType.SK_CATEGORY,
-    },
-  },
-  {
-    label: "Facebook",
-    value: {
-      id: 14,
-      type: AlertRuleType.MERCHANT,
-    },
-  },
-  {
-    label: "Apple",
-    value: {
-      id: 44,
-      type: AlertRuleType.MERCHANT,
-    },
-  },
-  {
-    label: "United Airlines",
-    value: {
-      id: 39,
-      type: AlertRuleType.MERCHANT,
-    },
-  },
-  {
-    label: "Engineering",
-    value: {
-      id: 14,
-      type: AlertRuleType.DEPARTMENT,
-    },
-  },
-  {
-    label: "Marketing",
-    value: {
-      id: 4,
-      type: AlertRuleType.DEPARTMENT,
-    },
-  },
-  {
-    label: "Sales",
-    value: {
-      id: 32,
-      type: AlertRuleType.DEPARTMENT,
-    },
-  },
-];
+export const useListSelectOptions = (): OptionValue[] =>
+  useMemo(
+    () => [
+      {
+        label: "Travel",
+        value: {
+          id: 14,
+          type: AlertRuleType.SK_CATEGORY,
+        },
+      },
+      {
+        label: "Electronics",
+        value: {
+          id: 19,
+          type: AlertRuleType.SK_CATEGORY,
+        },
+      },
+      {
+        label: "SaaS",
+        value: {
+          id: 92,
+          type: AlertRuleType.SK_CATEGORY,
+        },
+      },
+      {
+        label: "Facebook",
+        value: {
+          id: 14,
+          type: AlertRuleType.MERCHANT,
+        },
+      },
+      {
+        label: "Apple",
+        value: {
+          id: 44,
+          type: AlertRuleType.MERCHANT,
+        },
+      },
+      {
+        label: "United Airlines",
+        value: {
+          id: 39,
+          type: AlertRuleType.MERCHANT,
+        },
+      },
+      {
+        label: "Engineering",
+        value: {
+          id: 14,
+          type: AlertRuleType.DEPARTMENT,
+        },
+      },
+      {
+        label: "Marketing",
+        value: {
+          id: 4,
+          type: AlertRuleType.DEPARTMENT,
+        },
+      },
+      {
+        label: "Sales",
+        value: {
+          id: 32,
+          type: AlertRuleType.DEPARTMENT,
+        },
+      },
+    ],
+    []
+  );
